205b746807bacdaf7dfd56b2a09aba13bb304d20
Anthony <anthonyargatoff@gmail.com>
2024-03-28 12:39:46 -0700

    Added function to dynamically create cards

--- flaskr/static/resources/scripts/mapScript.js
+++ flaskr/static/resources/scripts/mapScript.js
@@ -13,7 +13,8 @@ document.addEventListener("DOMContentLoaded", async function(){
         const map = L.map('map', {worldCopyJump: true}).setView([0, 0], 3);
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 19, attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
 
-        events.forEach(event => {
+        events.forEach((event, index) => {
+
             const latitude = event.latitude;
             const longitude = event.longitude;
             const magnitude = event.magnitude;
@@ -24,9 +25,43 @@ document.addEventListener("DOMContentLoaded", async function(){
 
             marker = L.marker([latitude, longitude]).addTo(map);
             marker.bindPopup("Title: " + title + "<br>Event time: " + eventTime + "<br>Magnitude: " + magnitude + "<br>Latitude: " + latitude + "<br>Longitude: " + longitude + "<br>Depth: " + depth + "<br>URL: <a href='" + url + "' target='_blank'>Event Link</a>");
+
+            createCard("displayResults", event, index);
         })
 
     } catch (e){
         console.error("Error fetching earthquake data: ", e);
     }
 });
+
+
+/**
+ * 
+ * @param {string} elementId 
+ * @param {event} eventObj 
+ * @param {int} index 
+ */
+function createCard (elementId, eventObj, index) {
+    const newCard = document.createElement("a");
+    newCard.href = eventObj.url;
+    newCard.target = "_blank";
+    newCard.className = isEven(index) ? "list-group-item list-group-item-action active py-3 lh-sm" : "list-group-item list-group-item-action py-3 lh-sm";
+    newCard.setAttribute("aria-current", "true");
+    newCard.innerHTML = `<div class="d-flex w-100 align-items-center justify-content-between">
+    <strong class="mb-1">${eventObj.title}</strong>
+    <small>${eventObj.eventTime}</small>
+  </div>
+  <div class="mb-1 small">${eventObj.url}</div>`;
+
+    const node = document.getElementById(elementId);
+    node.appendChild(newCard);
+}
+
+/**
+ * 
+ * @param {int} n 
+ * @returns {bool} boolean
+ */
+function isEven (n){
+    return n % 2 == 0;
+}
