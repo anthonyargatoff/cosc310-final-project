8ddeb9d8d62566be6afa5e84a9c4be5839fdb158
Varun2711 <rvjpatel99@gmail.com>
2024-04-01 16:15:32 -0700

    added routing for signup and account management pages

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -9,22 +9,28 @@ auth = Blueprint('auth', __name__)
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     # check for post request
-        if request.method == 'POST':
-            email = request.form['email']
-            pw = request.form['password']
-            print(email, pw)
-            # need to figure out how to access the remember me checkbox
-            # next steps are to incorporate the database and setup credential validation
-            # from here redirect to the main page which is search page
-            # next steps for the redirect would be to include a payload to dynamically display public user data like their username
-            return redirect('/search')
+    if request.method == 'POST':
+        email = request.form['email']
+        pw = request.form['password']
+        print(email, pw)
+        # need to figure out how to access the remember me checkbox
+        # next steps are to incorporate the database and setup credential validation
+        # from here redirect to the main page which is search page
+        # next steps for the redirect would be to include a payload to dynamically display public user data like their username
+        return redirect('/search')
         
-        return render_template('login.html')
+    return render_template('login.html')
 
 @auth.route('/admin')
 def admin_page():
     return render_template('Admin.html')
 
-@auth.route('/accountmanagement')
-def accountmanager_page():
-    return render_template('accountManagement.html')
+# @auth.route('/accountmanagement')
+# def accountmanager_page():
+#     return render_template('accountManagement.html')
+
+@auth.route('/signup', methods =['GET', 'POST'])
+def signup_page():
+     if request.method == 'POST':
+         return redirect('/search')
+     return render_template('Signup.html')
