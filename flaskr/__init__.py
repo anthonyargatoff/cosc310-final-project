d5d1f526b4f56f850cba9db38adeb13330db0b98
RyanPybus <ryanpybus8596@gmail.com>
2024-04-01 18:05:39 -0700

    Update __init__.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -26,6 +26,7 @@ def create_app(test_config=None):
     from .auth import auth
     from .view import view
     from .notifications_routes import notifications
+
     
     # register blueprints to the app
     app.register_blueprint(auth, url_prefix = '/')
