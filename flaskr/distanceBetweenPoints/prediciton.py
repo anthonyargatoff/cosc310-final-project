4f632c6888930070e4603b3e6c9add1b0bb575f8
RyanPybus <ryanpybus8596@gmail.com>
2024-04-01 17:04:38 -0700

    Class for calculating prediction

---
+++ flaskr/distanceBetweenPoints/prediciton.py
@@ -0,0 +1,75 @@
+
+import sqlite3
+import distanceBetweenPoints as DBP
+from operator import attrgetter, itemgetter
+
+class predictionCalculator():
+
+    @staticmethod
+    def getEvents(startDate, endDate):
+        """Gets all events in date range from main.db 
+
+        Args:
+            startDate (String): _description_
+            endDate (String): _description_
+
+        Returns:
+            _type_: list of events with latitude and longitude keys for each event
+        """
+        con = sqlite3.connect("../main.db");
+        cursor = con.cursor();
+        params = (startDate, endDate)
+        SQL = "Select latitude, longitude From earthquake Where eventTime Between ? and ?;";
+        cursor.execute(SQL, params);
+        rows = cursor.fetchall();
+        con.commit();
+        con.close();
+
+        events = [];
+        for row in rows:
+            event = {'latitude': row[0], 'longitude': row[1]};
+            events.append(event)
+        return events;
+
+    @staticmethod
+    def getPredictions(events, radius = 100):
+        """_summary_
+
+        Args:
+            events (List<Events>): List of events with latitude and longitude keys
+            radius (number):    Radius (in km) around which to conduct the search, default 100.
+
+        Returns:
+            _type_: List of predictions (length < 10), have latitude and longitude keys, as well as a count key
+                        for the number of events nearby.
+        """
+        processedEvents = [];
+        for event in events:
+            reference = event;
+            count = 0;
+            for eachevent in events:
+                if DBP.coordinateCalculator.getDistanceKilometers(reference['latitude'],reference['longitude'],eachevent['latitude'],eachevent['longitude']) < radius:
+                    count = count+1;
+            processedEvents.append({'latitude': reference['latitude'], 'longitude': reference['longitude'], 'count':count});
+
+        sortedarr = sorted(processedEvents, key=itemgetter('count'), reverse=True);
+        output = [];
+        for processedEvent in sortedarr:
+            valid = True;
+            for eachevent in output:
+                if DBP.coordinateCalculator.getDistanceKilometers(processedEvent['latitude'],processedEvent['longitude'],eachevent['latitude'],eachevent['longitude']) < radius:
+                    valid = False;
+            if valid:
+                output.append(processedEvent);
+            if len(output) > 9:
+                return output;
+        return output;
+
+'''
+get point i of n from database.
+variable iPoints to hold number of points found
+compare it with all other database items, i2, i3, i4, ..., in
+if $in$ is in the radius, add +1 to iPoints
+repeat for each point in db
+return the top 10 points that do not overlap with each other
+'''
---
+++ flaskr/distanceBetweenPoints/temptest.py
@@ -0,0 +1,39 @@
+import prediciton
+import unittest
+
+class test_Prediction(unittest.TestCase):
+
+    def setUp(self):
+        self.testdata = [{'latitude': 61.6795, 'longitude': -143.2598}, {'latitude': -5.2583, 'longitude': 134.2234}, 
+                         {'latitude': -28.9513, 'longitude': -176.3111}, {'latitude': 60.6494, 'longitude': -151.4802}, 
+                         {'latitude': 59.8559, 'longitude': -150.5184}, {'latitude': 32.9043333, 'longitude': -115.5225}, 
+                         {'latitude': 11.6018, 'longitude': 57.6959}, {'latitude': 67.7689, 'longitude': -157.4438}, 
+                         {'latitude': 33.477, 'longitude': -116.5283333}, {'latitude': 61.1781, 'longitude': -146.7708}, 
+                         {'latitude': 38.7961655, 'longitude': -122.7426682}, {'latitude': -42.8557, 'longitude': -16.0022}, 
+                         {'latitude': 59.8906, 'longitude': -139.4907}, {'latitude': 32.901, 'longitude': -115.5228333}, 
+                         {'latitude': 44.6095, 'longitude': -112.9396667}, {'latitude': 38.7668343, 'longitude': -122.7109985}];
+        
+        self.expected = [{'count': 2, 'latitude': 32.9043333, 'longitude': -115.5225},
+                           {'count': 2, 'latitude': 38.7961655, 'longitude': -122.7426682},
+                           {'count': 1, 'latitude': 61.6795, 'longitude': -143.2598},
+                           {'count': 1, 'latitude': -5.2583, 'longitude': 134.2234},
+                           {'count': 1, 'latitude': -28.9513, 'longitude': -176.3111},
+                           {'count': 1, 'latitude': 60.6494, 'longitude': -151.4802},
+                           {'count': 1, 'latitude': 59.8559, 'longitude': -150.5184},
+                           {'count': 1, 'latitude': 11.6018, 'longitude': 57.6959},
+                           {'count': 1, 'latitude': 67.7689, 'longitude': -157.4438},
+                           {'count': 1, 'latitude': 33.477, 'longitude': -116.5283333}];
+        
+    def testGetEvents(self):
+        Events = prediciton.predictionCalculator.getEvents('2022-04-20','2022-04-21');
+        self.assertEqual(len(Events), 391);
+        self.assertEqual(Events[0], {'latitude': 61.6795, 'longitude': -143.2598});
+
+    def testGetPredictions(self):
+        preds = prediciton.predictionCalculator.getPredictions(self.testdata);
+        self.assertEqual(preds,self.expected)
+
+    def tearDown(self):
+        print();
+
+unittest.main();
