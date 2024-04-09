ffbcc08901cc4979d64b444188d0caf649f6c50a
Varun2711 <rvjpatel99@gmail.com>
2024-04-09 05:51:55 -0700

    made some changes

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -27,22 +27,15 @@ def login():
 def admin_page():
     return render_template('Admin.html')
 
-@auth.route('/signup', methods =['GET', 'POST'])
-def signup_page():
-    # process the form data
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
     if request.method == 'POST':
         email = request.form['email']
         pw = request.form['password']
-        cpw = request.form['confirm_password']
-
-        # check if the password and confirm password match and if the email is already in use
-        if hash(pw) != hash(cpw) or userDB.selectUserId(email) != False:
-            return render_template('Signup.html')
-        else:
-            # else create a new user and redirect to login page with code 307
-            userDB.addUser(email, pw, 0)
-            return redirect('/login', code=307)
-    return render_template('Signup.html')
+        confirmpw = request.form['confirm_password']
+        User = userDB('main.db')
+        if ((not User.validateUser()) and pw == confirmpw):
+            User.addUser(email, pw, 0)
 
 @auth.route('/account')
 def accountmanager_page():
